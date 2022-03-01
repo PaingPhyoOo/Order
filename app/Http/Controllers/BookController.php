e19053b4719717e2d897a9888feb6eed9ea7837d
Paing Phyo Oo <paingphyooo135@gmail.com>
2022-03-01 10:32:27 +0630

    remind bookcontroller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,7 +13,7 @@ class BookController extends Controller
     public function store(Bookrequest $request){
         Book::create([
             'name'=>$request->get('name'),
-            'tel-no'=>$request->get('tel'),
+            'tel_no'=>$request->get('tel'),
             'address'=>$request->get('adss'),
             'book_name'=>$request->get('bname')
         ]);
